1ddf22b26387d2f6e8c267b2019d4f16f6df9c34
Tony Lownds <tony@lownds.com>
2023-05-17 13:34:46 -0700

    Fix API urls, CLI with retries and base url

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 const fs = require('fs');
 const axios = require('axios');
+const fncli = require('fncli');
+
+let apiKey
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 // Function to generate alt text using alttext.ai API
 async function generateAltText(imageUrl) {
-  const apiKey = 'YOUR_API_KEY_HERE';
-  const apiUrl = 'https://api.alttext.ai/v1/generate';
-
-  try {
-    const response = await axios.post(apiUrl, {
-      image_url: imageUrl,
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-API-Key': apiKey,
-      },
-    });
-
-    return response.data.alt_text;
-  } catch (error) {
-    console.error('Error generating alt text:', error.response.data);
+  if (!apiKey) {
+    console.log("No api key! Can't generate alt text for", imageUrl);
     return null;
   }
+
+  const apiUrl = 'https://alttext.ai/api/v1/images';
+
+  let retries = 0;
+  do {
+    try {
+      const response = await axios.post(apiUrl, {
+        image: {url: imageUrl}
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': apiKey,
+        },
+      });
+
+      console.log('->', response.data);
+
+      return response.data.alt_text;
+    } catch (error) {
+      console.error('Error generating alt text:', error, error?.response?.data);
+      if (error?.response?.status == 429) {
+        // retry later
+        console.log("Retrying in 1 second");
+        await wait(1000);
+        retries++;
+      } else {
+        return null;
+      }
+    }
+  } while(retries < 5);
 }
 
 // Function to add alt text to Markdown image tags
-async function addAltTextToMarkdownFile(filePath) {
+async function addAltTextToMarkdownFile(filePath, baseUrl) {
   try {
     const markdownContent = fs.readFileSync(filePath, 'utf-8');
 
@@ -34,29 +55,57 @@ async function addAltTextToMarkdownFile(filePath) {
 
     let match;
     while ((match = imageRegex.exec(markdownContent)) !== null) {
-      const originalTag = match[0];
-      const altText = await generateAltText(match[2]);
+      const [originalTag, originalAltText, imageUrl] = match;
+      if (originalAltText) {
+        // Already has alt text
+        // console.log(`${filePath} already has alt text: ${originalAltText} ${imageUrl}`);
+        continue;
+      }
+      let absoluteUrl;
+      try {
+        absoluteUrl = new URL(imageUrl, baseUrl).toString();
+      } catch(e) {
+        console.error(e);
+        console.log('Perhaps you need to set the --base-url?')
+        continue;
+      }
+      const altText = await generateAltText(absoluteUrl);
 
       if (altText) {
-        const updatedTag = originalTag.replace(
-          match[1],
-          `${match[1]} "${altText}"`
-        );
+        const updatedTag = `![${altText}](${imageUrl})`;
         updatedContent = updatedContent.replace(originalTag, updatedTag);
       } else {
         console.log(`Failed to generate alt text for: ${match[2]}`);
       }
     }
 
-    fs.writeFileSync(filePath, updatedContent, 'utf-8');
-    console.log(`Alt text added to ${filePath}`);
+    if (markdownContent === updatedContent) {
+      console.log(`No changes ${filePath}`);
+    } else {        
+      fs.writeFileSync(filePath, updatedContent, 'utf-8');
+      console.log(`Alt text added to ${filePath}`);
+    }
   } catch (error) {
     console.error('Error reading or updating file:', error);
   }
 }
 
-// Specify the Markdown file path
-const markdownFilePath = 'path/to/your/file.md';
-
-// Add alt text to Markdown file
-addAltTextToMarkdownFile(markdownFilePath);
+fncli(async ({a: alttextApiKey=process.env.ALTTEXT_API_KEY, u: baseUrl, test=false}, ...markdownFiles) => {
+  apiKey = alttextApiKey;
+  if (test) {
+    const apiUrl = 'https://alttext.ai/api/v1/account';
+    const response = await axios.get(apiUrl, {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': apiKey,
+        },
+    });   
+    console.log(response);
+  }
+  if (markdownFiles.length == 0) {
+    console.log('no files to process');
+  }
+  for (const markdownFile of markdownFiles) {
+    await addAltTextToMarkdownFile(markdownFile, baseUrl);
+  }
+})
